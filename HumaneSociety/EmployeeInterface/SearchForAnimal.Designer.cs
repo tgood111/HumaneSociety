f98f35c910e4c86b58a7b912339ddb6c3e1bae99
tgood111 <tgood111@live.com>
2017-09-05 18:49:37 -0500

    Added new search form

---
+++ HumaneSociety/EmployeeInterface/SearchForAnimal.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace HumaneSociety.EmployeeInterface
+{
+    partial class SearchForAnimal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.name = new System.Windows.Forms.TextBox();
+            this.Room = new System.Windows.Forms.NumericUpDown();
+            this.Type = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Search = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Room)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(141, 29);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(182, 31);
+            this.name.TabIndex = 0;
+            // 
+            // Room
+            // 
+            this.Room.Location = new System.Drawing.Point(141, 66);
+            this.Room.Name = "Room";
+            this.Room.Size = new System.Drawing.Size(120, 31);
+            this.Room.TabIndex = 1;
+            // 
+            // Type
+            // 
+            this.Type.Location = new System.Drawing.Point(141, 103);
+            this.Type.Name = "Type";
+            this.Type.Size = new System.Drawing.Size(182, 31);
+            this.Type.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Age:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Type:";
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(47, 168);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(115, 47);
+            this.Search.TabIndex = 7;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = true;
+            // 
+            // Cancel
+            // 
+            this.Cancel.Location = new System.Drawing.Point(208, 168);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(115, 47);
+            this.Cancel.TabIndex = 8;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // SearchForAnimal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 244);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Type);
+            this.Controls.Add(this.Room);
+            this.Controls.Add(this.name);
+            this.Name = "SearchForAnimal";
+            this.Text = "SearchForAnimal";
+            ((System.ComponentModel.ISupportInitialize)(this.Room)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.NumericUpDown Room;
+        private System.Windows.Forms.TextBox Type;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.Button Cancel;
+    }
+}
---
+++ HumaneSociety/EmployeeInterface/SearchForAnimal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HumaneSociety.EmployeeInterface
+{
+    public partial class SearchForAnimal : Form
+    {
+        public SearchForAnimal()
+        {
+            InitializeComponent();
+            Room.Maximum = 20;
+            Room.Minimum = 1;
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
