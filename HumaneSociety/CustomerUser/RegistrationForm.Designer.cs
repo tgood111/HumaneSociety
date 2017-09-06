b21370222a64f6b3a7c770dc94e657b58babbcb6
tgood111 <tgood111@live.com>
2017-09-05 20:58:01 -0500

    Simple registration implmentation

---
+++ HumaneSociety/CustomerUser/RegistrationForm.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace HumaneSociety.CustomerUser
+{
+    partial class RegistrationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.name = new System.Windows.Forms.TextBox();
+            this.Age = new System.Windows.Forms.NumericUpDown();
+            this.Number = new System.Windows.Forms.TextBox();
+            this.Address = new System.Windows.Forms.TextBox();
+            this.Description = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Register = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Age)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(190, 52);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(194, 31);
+            this.name.TabIndex = 0;
+            // 
+            // Age
+            // 
+            this.Age.Location = new System.Drawing.Point(190, 89);
+            this.Age.Name = "Age";
+            this.Age.Size = new System.Drawing.Size(120, 31);
+            this.Age.TabIndex = 6;
+            // 
+            // Number
+            // 
+            this.Number.Location = new System.Drawing.Point(190, 126);
+            this.Number.Name = "Number";
+            this.Number.Size = new System.Drawing.Size(194, 31);
+            this.Number.TabIndex = 7;
+            // 
+            // Address
+            // 
+            this.Address.Location = new System.Drawing.Point(190, 163);
+            this.Address.Name = "Address";
+            this.Address.Size = new System.Drawing.Size(194, 31);
+            this.Address.TabIndex = 8;
+            // 
+            // Description
+            // 
+            this.Description.Location = new System.Drawing.Point(12, 240);
+            this.Description.Name = "Description";
+            this.Description.Size = new System.Drawing.Size(569, 307);
+            this.Description.TabIndex = 9;
+            this.Description.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 25);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Phone Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(91, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 25);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Adress";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 25);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Description";
+            // 
+            // Register
+            // 
+            this.Register.Location = new System.Drawing.Point(12, 553);
+            this.Register.Name = "Register";
+            this.Register.Size = new System.Drawing.Size(569, 72);
+            this.Register.TabIndex = 15;
+            this.Register.Text = "Register";
+            this.Register.UseVisualStyleBackColor = true;
+            this.Register.Click += new System.EventHandler(this.Register_Click);
+            // 
+            // Cancel
+            // 
+            this.Cancel.Location = new System.Drawing.Point(12, 641);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(569, 72);
+            this.Cancel.TabIndex = 16;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // RegistrationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 846);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.Register);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Description);
+            this.Controls.Add(this.Address);
+            this.Controls.Add(this.Number);
+            this.Controls.Add(this.Age);
+            this.Controls.Add(this.name);
+            this.Name = "RegistrationForm";
+            this.Text = "RegistrationForm";
+            ((System.ComponentModel.ISupportInitialize)(this.Age)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.NumericUpDown Age;
+        private System.Windows.Forms.TextBox Number;
+        private System.Windows.Forms.TextBox Address;
+        private System.Windows.Forms.RichTextBox Description;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Register;
+        private System.Windows.Forms.Button Cancel;
+    }
+}
---
+++ HumaneSociety/CustomerUser/RegistrationForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HumaneSociety.CustomerUser
+{
+    public partial class RegistrationForm : Form
+    {
+        public RegistrationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Click(object sender, EventArgs e)
+        {
+            Adopter adopter = new Adopter();
+            adopter.Adopter_Name = name.Text;
+            adopter.Adopter_Address = Address.Text;
+            adopter.Adopter_Phone_Number = Number.Text;
+            adopter.Adopter_Age = (byte) Age.Value;
+            adopter.Adopter_Description = Description.Text;
+            AdopterDbHelper.AddAdopter(adopter);
+            //Spawn the new main menu?
+            Close();
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
