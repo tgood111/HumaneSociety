ce904214576ee9c2add483653cd15d585fd89cfe
tgood111 <tgood111@live.com>
2017-09-05 20:59:43 -0500

    Project files

--- HumaneSociety/Program.cs
+++ HumaneSociety/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HumaneSociety.CustomerUser;
 using HumaneSociety.EmployeeInterface;
 
 namespace HumaneSociety
@@ -18,6 +19,7 @@ namespace HumaneSociety
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new EmployeeMainMenu());
+            Application.Run(new CustomerMainMenu());
         }
     }
 }
